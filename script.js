9bbd2852f3a0e1b93f27f4c59e51820742440561
alessandrostacchiotti <alessandro.stacchiotti@corridoni-campana.edu.it>
2026-02-04 11:46:01 +0000

    Vers 1

---
+++ script.js
@@ -0,0 +1,8 @@
+var Notte=0
+if (Notte/2==0){
+    document.getElementById('Immg').src='Notte.png'
+    document.getElementById('uno').innerHTML='Notte'
+} else {    
+    document.getElementById('Immg').src='Giorno.png'
+    document.getElementById('uno').innerHTML='Giorno'
+}
